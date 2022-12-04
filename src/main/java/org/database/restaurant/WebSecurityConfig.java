8c5364373453393caa05922abb48b681f036226e
what-is-me <2454787428@qq.com>
2022-12-04 18:41:58 +0800

    权限控制设置为不起效

--- src/main/java/org/database/restaurant/WebSecurityConfig.java
+++ src/main/java/org/database/restaurant/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,7 +19,7 @@ import java.util.Collections;
 
 @Configuration
 @Slf4j
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)//TODO
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Qualifier("dataSource")
     @Autowired
@@ -40,9 +39,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //http.authorizeRequests().antMatchers("/files/*").permitAll()//设置匹配的资源放行
-        //        .and().formLogin();//剩余任何资源必须认证
-        super.configure(http);
+        //http.authorizeRequests().antMatchers("/files/*").permitAll()
+        //        .and().formLogin();
+        http.authorizeRequests().anyRequest().permitAll();//TODO
+        //super.configure(http);
         http.cors()
                 .configurationSource(corsConfigurationSource())
                 .and().csrf().disable();
@@ -60,19 +60,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-    /*@Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.setAllowCredentials(true);
-
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(source);
-    }*/
 
 }
 
