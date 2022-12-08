ece0286f72ca3c11ed0d6d06d9cfcb46e9448a38
3113631254@qq.com <3113631254@qq.com>
2022-12-08 16:27:42 +0800

    后厨添加差不多了

---
+++ src/main/resources/static/admin/order.js
@@ -0,0 +1,69 @@
+function login() {
+}
+
+function service() {
+    document.getElementById("hide_bg").style.display = "none"
+    document.getElementById("log_form").style.display = "none"
+}
+
+
+
+function addUser_show() {
+    let add_user_option = document.getElementById("add_user_option");
+    add_user_option.style.background = "#a79e9e";
+    let update_code_option = document.getElementById("update_code_option");
+    pay_option.style.background = "#ffffff";
+    let out = document.getElementById("out_option");
+    out.style.background = "#ffffff";
+    let swap = document.getElementById("swap_option");
+    swap.style.background = "#ffffff";
+
+    let order_food = document.getElementById("order_food");
+    let pay_food = document.getElementById("pay_food");
+    order_food.style.display = "";
+    pay_food.style.display = "none";
+}
+
+function updateCode_show() {
+    let order = document.getElementById("order_option");
+    order.style.background = "#ffffff";
+    let pay_option = document.getElementById("pay_option");
+    pay_option.style.background = "#a79e9e";
+    let out = document.getElementById("out_option");
+    out.style.background = "#ffffff";
+    let swap = document.getElementById("swap_option");
+    swap.style.background = "#ffffff";
+
+    let order_food = document.getElementById("order_food");
+    let pay_food = document.getElementById("pay_food");
+    order_food.style.display = "none";
+    pay_food.style.display = "";
+
+}
+
+function swap_show() {
+    let order = document.getElementById("order_option");
+    order.style.background = "#ffffff";
+    let pay_option = document.getElementById("pay_option");
+    pay_option.style.background = "#ffffff";
+    let out = document.getElementById("out_option");
+    out.style.background = "#ffffff";
+    let swap = document.getElementById("swap_option");
+    swap.style.background = "#a79e9e";
+}
+
+function out_show() {
+    let order = document.getElementById("order_option");
+    order.style.background = "#ffffff";
+    let pay_option = document.getElementById("pay_option");
+    pay_option.style.background = "#ffffff";
+    let out = document.getElementById("out_option");
+    out.style.background = "#a79e9e";
+    let swap = document.getElementById("swap_option");
+    swap.style.background = "#ffffff";
+    location.href = "login.html";
+}
+
+
+
+
